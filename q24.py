0a47c270c00f1a688fc1f2c3f098940a08f085fa
Smalli68 <155727786+Smalli68@users.noreply.github.com>
2025-09-14 23:34:18 +0530

    Add files via upload

---
+++ q24.py
@@ -0,0 +1,12 @@
+# Define the tuple
+my_tuple = (11, 22, 33, 44, 55)
+
+# Unpack the tuple into five variables
+var1, var2, var3, var4, var5 = my_tuple
+
+# Print the values of the variables
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
