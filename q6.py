cd9f1c39a6b34b82d4582401efb4950f053d5eb3
Smalli68 <155727786+Smalli68@users.noreply.github.com>
2025-09-14 23:30:21 +0530

    Add files via upload

---
+++ q6.py
@@ -0,0 +1,7 @@
+tuple1 = (11, 22, 33, 44, 55, 66)
+
+# Elements 44 and 55 are at index 3 and 4 respectively.
+# Slicing from index 3 up to (but not including) index 5 will extract them.
+tuple2 = tuple1[3:5] 
+
+print(tuple2)
