7b01bfbd57f9c761f52b28b4f2cc7fc4a0ab5b9f
Smalli68 <155727786+Smalli68@users.noreply.github.com>
2025-09-14 23:45:42 +0530

    Add files via upload

---
+++ q13.py
@@ -0,0 +1,10 @@
+# Create a tuple with numbers
+my_numbers_tuple = (10, 20, 30, 40, 50)
+
+# Print the entire tuple
+print("The entire tuple:", my_numbers_tuple)
+
+# Access and print a specific item from the tuple using its index
+# Tuples are zero-indexed, so the first item is at index 0, the second at index 1, and so on.
+print("The first item (index 0):", my_numbers_tuple[0])
+print("The third item (index 2):", my_numbers_tuple[2])
