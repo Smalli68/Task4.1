6462ad73106f2d1e065ed173f1eaf4b12c8d7467
Smalli68 <155727786+Smalli68@users.noreply.github.com>
2025-09-14 23:25:30 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,8 @@
+# Original tuple
+original_tuple = (10, 20, 30, 40, 50) 
+
+# Reverse the tuple using slicing
+reversed_tuple = original_tuple[::-1]
+
+# Print the reversed tuple
+print(reversed_tuple)
