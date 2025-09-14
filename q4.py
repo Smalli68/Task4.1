6c1e7bf855460a4d5773b011d35b6886cd87949c
Smalli68 <155727786+Smalli68@users.noreply.github.com>
2025-09-14 23:26:47 +0530

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,11 @@
+# Define the tuple
+my_tuple = (10, 20, 30, 40)
+
+# Unpack the tuple into four variables
+var1, var2, var3, var4 = my_tuple
+
+# Display each variable
+print(f"var1: {var1}")
+print(f"var2: {var2}")
+print(f"var3: {var3}")
+print(f"var4: {var4}")
